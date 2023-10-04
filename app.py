dfbcf316c890bb6b28306849a76bef7ed1029b70
Khaled Elagamy <khaled_yasser1234@yahoo.com>
2023-10-04 05:18:02 +0300

    final app.py

--- app.py
+++ app.py
@@ -15,14 +15,21 @@ app = Flask(__name__)
 # Custom filter
 app.jinja_env.filters["usd"] = usd
 
-# Configure session to use filesystem (instead of signed cookies)
+# Configuration for Flask-Session
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+app.config["SESSION_TYPE"] = "mongodb"  # Set the session type to use MongoDB
+
+client = MongoClient(os.getenv("DATA_URL"))
+
+# Configure MongoDB for Flask-Session
+app.config["SESSION_MONGODB"] = client  # Replace with your MongoDB connection details
+
 Session(app)
+
 
 # Configure CS50 Library to use SQLite database
 #db = SQL("sqlite:///student.db")
-client = MongoClient(os.getenv("DATA_URL"))
+#client = MongoClient(os.getenv("DATA_URL"))
 db = client['student']
 
 try:
